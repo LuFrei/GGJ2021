15937cf0649176c02ddbfb1fe9273dcafe4bc692
LuFrei <32626445+LuFrei@users.noreply.github.com>
2021-01-29 23:22:17 -0500

    added stuff
    Player data file
    Tied to controller.
    Re-learning C#

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,18 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Player))]
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private GameObject player; 
-
+    private Player player;
 
     // Start is called before the first frame update
     void Start(){
-        
+        player = GetComponent<Player>();
     }
 
     // Update is called once per frame
-    void Update(){
-        player.transform.Translate(new Vector2(Input.GetAxis("Horizontal"), 0));
+    void FixedUpdate(){
+        player.transform.Translate(new Vector2(Input.GetAxis("Horizontal"), 0) * Time.deltaTime * player.speed);
     }
 }
